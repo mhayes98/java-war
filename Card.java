87e1634c079d5756289641805915f58ee53f0e50
matt <mh.dev@pm.me>
2024-11-28 02:26:53 -0500

    Minor work

--- Card.java
+++ Card.java
@@ -1,5 +1,15 @@
 public class Card {
-    int value;
-    String suite;
+    public String[] card_values = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+    public String[] card_suites = {"Clubs", "Diamonds", "Hearts", "Spades"};
+    public int value;
+    public String suite;
+    public String full_card_info;
+
+
+    public void createCard(int value_index, int suite_index) {
+        value = value_index+=1;
+        suite = card_suites[suite_index];
+        full_card_info = String.format("%s of %s", card_values[value_index], suite);
+    }
 
 }
--- Deck.java
+++ Deck.java
@@ -1,3 +1,20 @@
-public class Deck {
+import java.util.ArrayList;
 
+public class Deck {
+    public int current_deck_size;
+    public int starting_card_count;
+
+    public void createDeckObject() {
+        ArrayList<Card> deck = new ArrayList<Card>();
+    }
+
+    public void fillDeckWithCards() {
+        for (int i = 0; i < 13; i++) {
+            for( int y = 0; y < 4; y++) {
+                Card card = new Card();
+                system.out.println(i, y);
+                card.createCard(i, y);
+            }
+        }
+    }
 }
